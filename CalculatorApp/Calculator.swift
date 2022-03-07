b375a8b55bd703ffc7145b6d19e076a338c21d69
Erluan Felix Batista <erluan@gmail.com>
2022-03-07 19:58:23 -0300

    :hammer: project init

--- CalculatorApp/Calculator.swift
+++ CalculatorApp/Calculator.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class Calculator {
+    
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a+b
+    }
+    
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a-b
+    }
+    
+}
